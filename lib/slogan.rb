239509a53ffaf80118e85dd92e1307b0e3a3ca7f
Chris VanHaren <vanharen@gmail.com>
2020-03-24 01:38:40 -0500

    adding a bit of fun, in a common page header

---
+++ lib/slogan.rb
@@ -0,0 +1,11 @@
+class Slogan
+  VERBS = %w{Creating Developing Mandating Building Usurping}
+  WHEN  = %w{tomorrow today yesterday}
+  WHAT  = %w{technology solutions genetics }
+  ADJS  = %w{brighter sunnier happier dominant}
+
+  def madlibs
+    verb, what, time1, time2, time3, adj = [VERBS, WHAT, WHEN, WHEN, WHEN, ADJS].map(&:sample)
+    "#{verb} #{time1}'s #{what} #{time2}, for a #{adj} #{time3}"
+  end
+end
