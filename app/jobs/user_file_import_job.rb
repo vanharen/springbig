eabc965e00f71271a53d358952d24c991f3305c7
Chris VanHaren <vanharen@gmail.com>
2020-03-26 17:05:34 -0500

    move string validation methods to String class, makes code easier to read and test

--- app/jobs/user_file_import_job.rb
+++ app/jobs/user_file_import_job.rb
@@ -23,8 +23,8 @@ class UserFileImportJob < ApplicationJob
       errors = []
 
       first, last = user_hash[:first], user_hash[:last]
-      errors << "Invalid first name"    if !name_valid?(first)
-      errors << "Invalid last name"     if !name_valid?(last)
+      errors << "Invalid first name"    if !first.valid_name?
+      errors << "Invalid last name"     if !last.valid_name?
       
       # - Neither first name or last name are required, however, the
       #   last name cannot be specified if first name is not present.
@@ -32,60 +32,16 @@ class UserFileImportJob < ApplicationJob
         
       phone = user_hash[:phone]
       phone.gsub!(/[-\.\(\)]/, '')            # Strip dash, dot, parens
-      errors << "Invalid phone number"  if !phone_valid?(phone)
+      errors << "Invalid phone number"  if !phone.valid_phone?
 
-      errors << "Invalid email address" if !email_valid?(user_hash[:email])
+      email = user_hash[:email]
+      errors << "Invalid email address" if !email.valid_email?
 
       # Any errors to report?
       user_hash[:error_string] = errors.join(", ") if !errors.empty?
 
       user_file.users.create(user_hash)
     }
-  end
-
-  private
-
-  # - Phone numbers may contain characters 0-9,-,.(,). Punctuation
-  #   need not adhere to a particular format or match,
-  #   e.g. “123.456-8888” and “87644.45523” are both legal.
-  # - Phone numbers must contain exactly 10 digits. Assuming
-  #   0-origin indexing, positions 0 and 3 may not contain
-  #   characters ‘0’ or ‘1’.
-  def phone_valid?(phone)
-    /^[0-9]{10}$/.match?(phone) &&
-      ([phone[0],phone[3]] & ["0","1"]).empty?  # Union empty?
-  end
-      
-  # - Email addresses must be standard and valid.
-  #   For this we will *mostly* follow RFC 3696 rules
-  #     (see: https://en.wikipedia.org/wiki/Email_address,
-  #           https://tools.ietf.org/html/rfc3696)
-  #   - Must match "local_part"@"domain"
-  #   - We will ignore quoted addresses, which have extended rules
-  #   - "local_part" may consist of alphanumerics plus "!#$%&'*+-/=?^_`.{|}~"
-  #   - for simplicity we will ignore the requirement that period
-  #     not be first or last, nor two periods in a row
-  #   - "local_part" must be <= 64 chars
-  #   - "domain" must be <= 255 chars and consist of alphanumerics plus "-"
-  #   - "domain" must contain at least one period in the middle of the string
-  #   - for simplicity we will ignore the requirement that hyphen
-  #     not be first or last, nor that the TLD be not all-numeric
-  def email_valid?(email)
-    parts = email.split("@")
-    return false if parts.size != 2
-
-    local_part, domain = parts
-
-    return false if domain.split(".").size < 2 # must have one period
-
-    /^[\w!\#$%&'*+-\/=?^_`.{\|}~]{1,64}$/.match?(local_part) &&
-      /^[A-Za-z0-9\-\.]{1,255}$/.match?(domain)
-  end
-
-  # - Names must either be nil, or if non-nil must be alpha only and
-  #   at least 2 characters long
-  def name_valid?(name)
-    name.nil? || /^[A-Za-z]{2,}$/.match?(name)
   end
 
 end
---
+++ lib/string_extras.rb
@@ -0,0 +1,47 @@
+# coding: utf-8
+class String
+
+  # - Phone numbers may contain characters 0-9,-,.(,). Punctuation
+  #   need not adhere to a particular format or match,
+  #   e.g. “123.456-8888” and “87644.45523” are both legal.
+  # - Phone numbers must contain exactly 10 digits. Assuming
+  #   0-origin indexing, positions 0 and 3 may not contain
+  #   characters ‘0’ or ‘1’.
+  def valid_phone?
+    /^[0-9]{10}$/.match?(self) &&
+      ([self[0],self[3]] & ["0","1"]).empty?  # Union empty?
+  end
+      
+  # - Email addresses must be standard and valid.
+  #   For this we will *mostly* follow RFC 3696 rules
+  #     (see: https://en.wikipedia.org/wiki/Email_address,
+  #           https://tools.ietf.org/html/rfc3696)
+  #   - Must match "local_part"@"domain"
+  #   - We will ignore quoted addresses, which have extended rules
+  #   - "local_part" may consist of alphanumerics plus "!#$%&'*+-/=?^_`.{|}~"
+  #   - for simplicity we will ignore the requirement that period
+  #     not be first or last, nor two periods in a row
+  #   - "local_part" must be <= 64 chars
+  #   - "domain" must be <= 255 chars and consist of alphanumerics plus "-"
+  #   - "domain" must contain at least one period in the middle of the string
+  #   - for simplicity we will ignore the requirement that hyphen
+  #     not be first or last, nor that the TLD be not all-numeric
+  def valid_email?
+    parts = self.split("@")
+    return false if parts.size != 2
+
+    local_part, domain = parts
+
+    return false if domain.split(".").size < 2 # must have one period
+
+    /^[\w!\#$%&'*+-\/=?^_`.{\|}~]{1,64}$/.match?(local_part) &&
+      /^[A-Za-z0-9\-\.]{1,255}$/.match?(domain)
+  end
+
+  # - Names must either be nil, or if non-nil must be alpha only and
+  #   at least 2 characters long
+  def valid_name?
+    self.nil? || /^[A-Za-z]{2,}$/.match?(self)
+  end
+
+end
