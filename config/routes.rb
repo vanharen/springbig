3cf95378c35d05feab6a42473619fab44c73e0c5
Chris VanHaren <vanharen@gmail.com>
2020-03-27 12:16:27 -0500

    move Slogan to app/classes, and add that path to eager_load_paths. use sidekiq for ActiveJob in development and production mode, and add sidekiq web ui to routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 # For details on the DSL available within this file, see
 # https://guides.rubyonrails.org/routing.html
 
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   root 'user_files#index'
 
   resources :user_files, only: [:index, :show, :new, :create]
+
+  mount Sidekiq::Web => '/sidekiq'
 end
