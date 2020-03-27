10832808cdd322e995308fb3c7d08dc561d6cd97
Chris VanHaren <vanharen@gmail.com>
2020-03-26 22:36:06 -0500

    add [] method as shorthand for find_by_id()

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,10 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  # Nifty hack that lets us refer to ActiveRecord objects as if they
+  # were an array
+  def self.[](id)
+    find_by_id(id)
+  end
+
 end
